1bff02eddb190ab1c3fa08790abd30b8a3d10f19
anthonysea <anthonyqchao@gmail.com>
2013-07-15 17:45:25 -0700

    added icon

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -7,6 +7,10 @@ app.config.update(
 	DEBUG=True,
 )
 
+
+@app.route('/favicon.ico')
+def favicon():
+	return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
 
 @app.route("/")
 def hello():
